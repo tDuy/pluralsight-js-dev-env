d9c0e4eb618fb4c156c20a4b6061c7f7c3628f42
Duy Tu <tduy.2201@gmail.com>
2018-01-04 01:25:17 +0700

    deploy UI to surge, use glitch as backend API

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,5 +1,5 @@
 export default function getBaseUrl() {
-  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/';
+  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : 'https://sour-enemy.glitch.me/';
 }
 
 function getQueryStringParameterByName(name, url) {
