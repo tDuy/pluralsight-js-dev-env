2cb23f2bedfcc295338d35d0d4193f81b796be0f
Duy Tu <tduy.2201@gmail.com>
2018-01-03 13:58:15 +0700

    api centralizing http request; whatwg-fetch, polyfill; mocking http with json-server json-schema-faker

--- src/index.js
+++ src/index.js
@@ -1,7 +1,32 @@
+import { getUsers, deleteUser } from "./api/userApi";
 import './index.css';
 
-import numeral from "numeral";
+getUsers()
+  .then(result => {
+    let usersBody = "";
 
-const courseValue = numeral(1000).format('$0,0.00');
+    result.forEach(user => {
+      usersBody += `<tr>
+        <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+        <td>${user.id}</td>
+        <td>${user.firstName}</td>
+        <td>${user.lastName}</td>
+        <td>${user.email}</td>
+      </tr>`;
+    });
 
-console.log(`I would pay ${courseValue} for this awesome course!`); // eslint-disable-line no-console
+    global.document.getElementById('users').innerHTML = usersBody;
+
+    const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+    Array.from(deleteLinks, link => {
+      link.onclick = (event) => {
+        const element = event.target;
+        event.preventDefault();
+        deleteUser(element.dataset.id);
+        const row = element.parentNode.parentNode;
+        row.parentNode.removeChild(row);
+      };
+    });
+  })
+ .catch(error => console.log(error)); // eslint-disable-line no-console
--- src/index.test.js
+++ src/index.test.js
@@ -4,11 +4,11 @@ const { JSDOM } = jsdom;
 import fs from "fs";
 
 describe('index.html', () => {
-  it('should say hello', (done) => {
+  it('should have h1 that says Users', (done) => {
     const index = fs.readFileSync('./src/index.html', 'utf-8');
     const dom = new JSDOM(index);
     const h1 = dom.window.document.getElementsByTagName('h1')[0];
-    expect(h1.innerHTML).to.equal('Hello World!');
+    expect(h1.innerHTML).to.equal('Users');
     done();
     window.close();
   });
